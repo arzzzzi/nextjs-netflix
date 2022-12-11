980314d892bcb0db58659f30ed7ee9cd149dde7f
arzzzzi <kantyshevmagomed@gmail.com>
2022-12-11 20:07:51 +0300

    movies scroll styled rows and thumbnails

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home = ({
   documentaries,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[100vh] lg:w-[100vw]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[100vh] ">
       <Head>
         <title>Home | Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -35,7 +35,7 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="md:space-y-24">
           <Row title="Trending now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
